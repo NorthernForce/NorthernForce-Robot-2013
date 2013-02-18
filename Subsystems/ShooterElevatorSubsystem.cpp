259dec1bddab4718b2d0e7ad1c85125ec89ebc8c
Joseph Martin <josephm@maine.rr.om>
2013-02-18 14:57:19 -0500

    Fixed occurence of "GetManipulatorStick", which was missing "()" because it's a function

--- Subsystems/ShooterElevatorSubsystem.cpp
+++ Subsystems/ShooterElevatorSubsystem.cpp
@@ -35,8 +35,8 @@ float ShooterElevatorSubsystem::GetElevationAngle() {
     float angle = kElevatorDegreesPerVolt * (kElevatorZero - voltage);
 	if(m_loggingEnabled) {
 		char _tmp[55];
-		sprintf(_tmp, "Voltage: %5.3f, Angle: %5.3f deg, Shooter Speed %5.3f", voltage, angle, CommandBase::oi->GetManipulatorStick.GetPOT());
-		printf("Voltage: %5.3f, Angle: %5.3f deg, Shooter Speed %5.3f\n", voltage, angle, CommandBase::oi->GetManipulatorStick.GetPOT());
+		sprintf(_tmp, "Voltage: %5.3f, Angle: %5.3f deg, Shooter Speed %5.3f", voltage, angle, CommandBase::oi->GetManipulatorStick().GetPOT());
+		printf("Voltage: %5.3f, Angle: %5.3f deg, Shooter Speed %5.3f\n", voltage, angle, CommandBase::oi->GetManipulatorStick().GetPOT());
 		m_elevatorLog.Write(_tmp);
 	}	
 	return angle;
