96a944074e6fb58bea3ef4cfa1ee9c6e1e6c55b3
Arthur Lockman <hello@rthr.me>
2013-05-21 16:43:58 -0400

    Added a pulse based encoder to make it easier to deal with the shooter wheel pinup.

---
+++ lib/PulseBasedEncoder.cpp
@@ -0,0 +1,81 @@
+#include "PulseBasedEncoder.h"
+
+/**
+ * @brief Constructor for the Pulse encoder.
+ * @param channel The digital channel that the encoder is connected to.
+ * @param pulsesPerRev The number of pulses per one revolution. Default 1.
+ * @param distancePerPulse The distance traveled per each pulse. 
+ */
+PulseBasedEncoder::PulseBasedEncoder(int channel, int pulsesPerRev, double distancePerPulse) :
+	c(channel),
+	m_distPerPulse(distancePerPulse),
+	m_pulsesPerRev(pulsesPerRev)
+{
+	c.Start();
+}
+
+/**
+ * @brief Destroys the encoder object.
+ */
+PulseBasedEncoder::~PulseBasedEncoder()
+{
+	c.Stop();
+}
+
+/**
+ * @brief Returns the veolcity of the encoder.
+ * @return A double, the velocity in RPM.
+ */
+double PulseBasedEncoder::GetVelocity()
+{
+	double per = c.GetPeriod();
+	//@TODO: Verify this math.
+	return (per / 60.0) * (double)m_pulsesPerRev;
+}
+
+/**
+ * @brief Sets the distance traveled each pulse.
+ * @param distancePerPulse The distance traveled each pulse.  
+ */
+void PulseBasedEncoder::SetDistancePerPulse(double distancePerPulse)
+{
+	m_distPerPulse = distancePerPulse;
+}
+
+/**
+ * @brief Sets the number of pulses per one revolution.
+ * @param pulsesPerRev The number of pulses per each revolution. 
+ */
+void PulseBasedEncoder::SetPulsesPerRevolution(int pulsesPerRev)
+{
+	m_pulsesPerRev = pulsesPerRev;
+}
+
+/**
+ * @brief Gets the distance traveled by the encoder. WARNING: Does not know
+ * which direction the encoder has traveled.
+ * @return A double, the distance traveled. 
+ */
+double PulseBasedEncoder::GetDistance()
+{
+	return c.Get() * m_distPerPulse;
+}
+
+/**
+ * @brief Gets the most recent time per pulse.
+ * @return A double, the period in seconds of the last pulse.
+ */
+double PulseBasedEncoder::GetPeriod()
+{
+	return c.GetPeriod();
+}
+
+/**
+ * @brief Resets the encoder to zero.
+ */
+void PulseBasedEncoder::Reset()
+{
+	c.Stop();
+	c.Reset();
+	c.Start();
+}
---
+++ lib/PulseBasedEncoder.h
@@ -0,0 +1,26 @@
+#include <WPILib.h>
+
+/**
+ * An encoder that is based off of some pulse device, like a 
+ * proximity sensor connected to a bolt on a wheel. This encoder
+ * can calculate the velocity and distance traveled of the 
+ * encoder.
+ * 
+ * @author Arthur Lockman
+ */
+class PulseBasedEncoder
+{
+public:
+	PulseBasedEncoder(int channel, int pulsesPerRev = 1, double distancePerPulse = 1.0);
+	~PulseBasedEncoder();
+	double GetVelocity();
+	void SetDistancePerPulse(double distancePerPulse);
+	void SetPulsesPerRevolution(int pulsesPerRev);
+	double GetPeriod();
+	double GetDistance();
+	void Reset();
+private:
+	Counter c;
+	double m_distPerPulse;
+	int m_pulsesPerRev;
+};
